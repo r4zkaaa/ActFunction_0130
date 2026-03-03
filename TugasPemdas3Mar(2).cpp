6f6c270a9aba92d978c7198a783980eeb3bf362e
r4zkaaa <rzkiiahmd11@gmail.com>
2026-03-03 21:34:38 +0700

    teknik

--- TugasPemdas3Mar(2).cpp
+++ TugasPemdas3Mar(2).cpp
@@ -11,3 +11,11 @@ void input() {
     cout << "Masukkan lebar = ";
     cin >> l;
 }
+
+float jumlah(float a, float b, float c) {
+    return a + b + c;
+}
+
+int luasPersegiP(int a, int b) {
+    return a * b;
+}
