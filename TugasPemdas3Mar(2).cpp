598e4303c1fd141af61c287ab316bbc49d2de27e
r4zkaaa <rzkiiahmd11@gmail.com>
2026-03-03 21:33:48 +0700

    teknik

--- TugasPemdas3Mar(2).cpp
+++ TugasPemdas3Mar(2).cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 // deklarasi global
 int p, l;
+
+// Implementasi Prosedur dan Fungsi
+void input() {
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
