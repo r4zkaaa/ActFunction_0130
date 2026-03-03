a96cc98749ad9e4aa9385a5a56faaaf9b74c74db
r4zkaaa <rzkiiahmd11@gmail.com>
2026-03-03 21:36:12 +0700

    teknik

--- TugasPemdas3Mar(2).cpp
+++ TugasPemdas3Mar(2).cpp
@@ -24,3 +24,10 @@ void output() {
     cout << "Hasilnya = " << luasPersegiP(p, l) << endl;
     cout << "terima kasih" << endl;
 }
+int main() { //mulai
+    input();
+    output();
+    cout << "hasil penjumlahan = " << jumlah(15.1, 24.9, 67) << endl;
+    
+    return 0;
+} //selesai
